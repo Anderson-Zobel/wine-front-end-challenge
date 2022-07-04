e94916758289c483ce35efcaf6d2d1cd1d191c87
Anderson-Zobel <anderson.zobels@gmail.com>
2022-07-04 04:06:19 -0300

    feat: 🛠 general styles for RadioFilter component

---
+++ src/styles/components/radiofilter.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Aside = styled.aside`
+  font-weight: bold;
+  width: 20%;  
+`
+
+export const FilterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Label = styled.label`
+  font-weight: 400;
+  padding: 10px;
+`
